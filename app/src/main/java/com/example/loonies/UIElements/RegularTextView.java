18549ebd5c753792bbee95fe7bc3c696d4fbd2b9
Tülin Kuzulu <tulin.kuzulu@gmail.com>
2019-07-26 14:01:32 -0400

    Fonts, colors, some other res files are added plus login activity layout added.

---
+++ app/src/main/java/com/example/loonies/UIElements/RegularTextView.java
@@ -0,0 +1,30 @@
+package com.example.loonies.UIElements;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class RegularTextView extends TextView {
+
+    public RegularTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public RegularTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RegularTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Montserrat-Regular.ttf");
+        setTypeface(tf);
+    }
+
+}
